d27bd1471c1b320603594a2e9fa4bf12389ca2c6
Rom3333 <32419103+Rom3333@users.noreply.github.com>
2017-12-26 23:13:11 +0300

    Parallel

--- fragmentation.cpp
+++ fragmentation.cpp
@@ -1,19 +1,17 @@
-#include "fragmentation.h"
+п»ї#include "fragmentation.h"
 #include <fstream>
 
 
-using namespace std;
-/// вектор, содержащий box-ы, являющиеся частью рабочего пространства
-vector<Box> solution;
-/// вектор, содержащий box-ы, не являющиеся частью рабочего пространства
-vector<Box> not_solution;
-/// вектор, содержащий box-ы, находящиеся на границе между "рабочим" и "нерабочим" пространством
-vector<Box> boundary;
-/// вектор, хранящий box-ы, анализируемые на следующей итерации алгоритма
-vector<Box> temporary_boxes;
 
 using namespace std;
-/// функции gj()
+
+cilk::reducer<cilk::op_vector<Box>>solution;
+cilk::reducer<cilk::op_vector<Box>>not_solution;
+cilk::reducer<cilk::op_vector<Box>>boundary;
+cilk::reducer<cilk::op_vector<Box>>temporary_boxes;
+
+using namespace std;
+/// ГґГіГ­ГЄГ¶ГЁГЁ gj()
 
 //------------------------------------------------------------------------------------------
 double g1(double x1, double x2)
@@ -49,16 +47,16 @@ low_level_fragmentation::low_level_fragmentation(const Box& box)
 //------------------------------------------------------------------------------------------
 void low_level_fragmentation::VerticalSplitter(const Box& box, boxes_pair& vertical_splitter_pair)
 {
-	// Определили функцию
+	// ГЋГЇГ°ГҐГ¤ГҐГ«ГЁГ«ГЁ ГґГіГ­ГЄГ¶ГЁГѕ
 	double x, y, w, h;
 	box.GetParameters(x, y, w, h);
 	vertical_splitter_pair.first = Box(x, y, w / 2, h);
-	vertical_splitter_pair.second = Box(x + w / 2, y, w / 2, h);	
+	vertical_splitter_pair.second = Box(x + w / 2, y, w / 2, h);
 }
 //------------------------------------------------------------------------------------------
 void low_level_fragmentation::HorizontalSplitter(const Box& box, boxes_pair& horizontal_splitter_pair)
 {
-	// Определили функцию
+	// ГЋГЇГ°ГҐГ¤ГҐГ«ГЁГ«ГЁ ГґГіГ­ГЄГ¶ГЁГѕ
 	double x, y, w, h;
 	box.GetParameters(x, y, w, h);
 	horizontal_splitter_pair.first = Box(x, y, w, h / 2);
@@ -67,7 +65,7 @@ void low_level_fragmentation::HorizontalSplitter(const Box& box, boxes_pair& hor
 //------------------------------------------------------------------------------------------
 void low_level_fragmentation::GetNewBoxes(const Box& box, boxes_pair& new_pair_of_boxes)
 {
-	// Определили функцию
+	// ГЋГЇГ°ГҐГ¤ГҐГ«ГЁГ«ГЁ ГґГіГ­ГЄГ¶ГЁГѕ
 	double x, y, w, h;
 	box.GetParameters(x, y, w, h);
 	if (w >= h)
@@ -86,7 +84,7 @@ unsigned int low_level_fragmentation::FindTreeDepth()
 	else
 	{
 		boxes_pair new_boxes;
-		// допустим, разобьем начальную область по ширине
+		// Г¤Г®ГЇГіГ±ГІГЁГ¬, Г°Г Г§Г®ГЎГјГҐГ¬ Г­Г Г·Г Г«ГјГ­ГіГѕ Г®ГЎГ«Г Г±ГІГј ГЇГ® ГёГЁГ°ГЁГ­ГҐ
 		VerticalSplitter(current_box, new_boxes);
 		unsigned int tree_depth = 1;
 		box_diagonal = new_boxes.first.GetDiagonal();
@@ -115,27 +113,27 @@ unsigned int low_level_fragmentation::FindTreeDepth()
 //------------------------------------------------------------------------------------------
 int low_level_fragmentation::ClasifyBox(const min_max_vectors& vects)
 {
-	// Определили функцию
+	// ГЋГЇГ°ГҐГ¤ГҐГ«ГЁГ«ГЁ ГґГіГ­ГЄГ¶ГЁГѕ
 	int maxCount = 0;
 	int minCount = 0;
 	int firstL = vects.first.size();
 	int secondL = vects.second.size();
 
-	if (vects.first[0] == 0) return 3; //Граница прямоугольника
+	if (vects.first[0] == 0 && vects.second[0] == 0) return 3;  //ГѓГ°Г Г­ГЁГ¶Г  ГЇГ°ГїГ¬Г®ГіГЈГ®Г«ГјГ­ГЁГЄГ 
 
 	for (int i = 0; i < 4; i++)
 	{
-		if (vects.first[i] < 0) maxCount++; //Проверка выполнения условия 4
-		if (vects.second[i] > 0) return 0; //Проверка выполнения условия 5
+		if (vects.first[i] < 0) maxCount++; //ГЏГ°Г®ГўГҐГ°ГЄГ  ГўГ»ГЇГ®Г«Г­ГҐГ­ГЁГї ГіГ±Г«Г®ГўГЁГї 4
+		if (vects.second[i] > 0) return 0; //ГЏГ°Г®ГўГҐГ°ГЄГ  ГўГ»ГЇГ®Г«Г­ГҐГ­ГЁГї ГіГ±Г«Г®ГўГЁГї 5
 	}
 
 	if (maxCount == 4) return 1;
-	else return 2; //Разбиваем на 2 новых		
+	else return 2; //ГђГ Г§ГЎГЁГўГ ГҐГ¬ Г­Г  2 Г­Г®ГўГ»Гµ		
 }
 //------------------------------------------------------------------------------------------
 void low_level_fragmentation::GetBoxType(const Box& box)
 {
-	// Определили функцию
+	// ГЋГЇГ°ГҐГ¤ГҐГ«ГЁГ«ГЁ ГґГіГ­ГЄГ¶ГЁГѕ
 
 	int type;
 	min_max_vectors vecs;
@@ -144,20 +142,20 @@ void low_level_fragmentation::GetBoxType(const Box& box)
 
 	type = ClasifyBox(vecs);
 
-	if (type == 0) //Убираем прямоугольник из рассмотрения
-		not_solution.push_back(box);
+	if (type == 0) //Г“ГЎГЁГ°Г ГҐГ¬ ГЇГ°ГїГ¬Г®ГіГЈГ®Г«ГјГ­ГЁГЄ ГЁГ§ Г°Г Г±Г±Г¬Г®ГІГ°ГҐГ­ГЁГї
+		not_solution->push_back(box);
 
-	if (type == 1) //Прямоугольник, в котором есть решение (нужный экстремум)
-		solution.push_back(box);
+	if (type == 1) //ГЏГ°ГїГ¬Г®ГіГЈГ®Г«ГјГ­ГЁГЄ, Гў ГЄГ®ГІГ®Г°Г®Г¬ ГҐГ±ГІГј Г°ГҐГёГҐГ­ГЁГҐ (Г­ГіГ¦Г­Г»Г© ГЅГЄГ±ГІГ°ГҐГ¬ГіГ¬)
+		solution->push_back(box);
 
-	if (type == 2) { //Прямоугольник, который надо разбить
+	if (type == 2) { //ГЏГ°ГїГ¬Г®ГіГЈГ®Г«ГјГ­ГЁГЄ, ГЄГ®ГІГ®Г°Г»Г© Г­Г Г¤Г® Г°Г Г§ГЎГЁГІГј
 		GetNewBoxes(box, pair);
-		temporary_boxes.push_back(pair.first);
-		temporary_boxes.push_back(pair.second);
+		temporary_boxes->push_back(pair.first);
+		temporary_boxes->push_back(pair.second);
 	}
 
-	if (type == 3) //Пригр
-		boundary.push_back(box);
+	if (type == 3) //ГѓГ°Г Г­ГЁГ¶Г 
+		boundary->push_back(box);
 }
 //------------------------------------------------------------------------------------------
 high_level_analysis::high_level_analysis(double& min_x, double& min_y, double& x_width, double& y_height) :
@@ -193,43 +191,43 @@ void high_level_analysis::GetMinMax(const Box& box, min_max_vectors& min_max_vec
 	}
 
 	// MIN
-	// функция g1(x1,x2)
+	// ГґГіГ­ГЄГ¶ГЁГї g1(x1,x2)
 	a1min = __min(abs(xmin), abs(xmax));
 	a2min = __min(abs(ymin), abs(ymax));
 	g_min.push_back(g1(a1min, a2min));
 
-	// функция g2(x1,x2)
+	// ГґГіГ­ГЄГ¶ГЁГї g2(x1,x2)
 	a1min = __max(abs(xmin), abs(xmax));
 	a2min = __max(abs(ymin), abs(ymax));
 	g_min.push_back(g2(a1min, a2min));
 
-	// функция g3(x1,x2)
+	// ГґГіГ­ГЄГ¶ГЁГї g3(x1,x2)
 	a1min = __min(abs(xmin - g_l0), abs(xmax - g_l0));
 	a2min = __min(abs(ymin), abs(ymax));
 	g_min.push_back(g3(a1min, a2min));
 
-	// функция g4(x1,x2)
+	// ГґГіГ­ГЄГ¶ГЁГї g4(x1,x2)
 	a1min = __max(abs(xmin - g_l0), abs(xmax - g_l0));
 	a2min = __max(abs(ymin), abs(ymax));
 	g_min.push_back(g4(a1min, a2min));
 
 	// MAX
-	// функция g1(x1,x2)
+	// ГґГіГ­ГЄГ¶ГЁГї g1(x1,x2)
 	a1max = __max(abs(xmin), abs(xmax));
 	a2max = __max(abs(ymin), abs(ymax));
 	g_max.push_back(g1(a1max, a2max));
 
-	// функция g2(x1,x2)
+	// ГґГіГ­ГЄГ¶ГЁГї g2(x1,x2)
 	a1max = __min(abs(xmin), abs(xmax));
 	a2max = __min(abs(ymin), abs(ymax));
 	g_max.push_back(g2(a1max, a2max));
 
-	// функция g3(x1,x2)
+	// ГґГіГ­ГЄГ¶ГЁГї g3(x1,x2)
 	a1max = __max(abs(xmin - g_l0), abs(xmax - g_l0));
 	a2max = __max(abs(ymin), abs(ymax));
 	g_max.push_back(g3(a1max, a2max));
 
-	// функция g4(x1,x2)
+	// ГґГіГ­ГЄГ¶ГЁГї g4(x1,x2)
 	a1max = __min(abs(xmin - g_l0), abs(xmax - g_l0));
 	a2max = __min(abs(ymin), abs(ymax));
 	g_max.push_back(g4(a1max, a2max));
@@ -240,49 +238,52 @@ void high_level_analysis::GetMinMax(const Box& box, min_max_vectors& min_max_vec
 //------------------------------------------------------------------------------------------
 void high_level_analysis::GetSolution()
 {
-	//должны фигурировать два вложенных цикла. Внешний цикл проходит по всем уровням двоичного дерева разбиения. 
-	//В рамках внутреннего цикла происходит перебор всех box-ов текущего уровня разбиения и определение типа box-а 
-	//(является он частью рабочего пространства либо не является, лежит он на границе или подлежит дальнейшему анализу). 
-
+	// ГЋГЇГ°ГҐГ¤ГҐГ«ГЁГ«ГЁ ГґГіГ­ГЄГ¶ГЁГѕ
 	int length = FindTreeDepth() + 1;
 	boxes_pair pair;
-	temporary_boxes.push_back(current_box);
+	temporary_boxes->push_back(current_box);
 
-	for (int i = 0; i < length; i++) {
-		int number_of_box_on_level = temporary_boxes.size();
-		vector<Box> curr_boxes(temporary_boxes);
-		temporary_boxes.clear();
-		for (int j = 0; j < number_of_box_on_level; j++) {
+	for (int i = 0; i < length; i++)
+	{
+		vector<Box> tmp;
+		temporary_boxes.move_out(tmp);
+		int number_of_box_on_level = tmp.size();
+		vector<Box> curr_boxes(tmp);
+		tmp.clear();
+		temporary_boxes.set_value(tmp);
+		cilk_for(int j = 0; j < number_of_box_on_level; j++)
+		{
 			GetBoxType(curr_boxes[j]);
 		}
 	}
 }
+
 //------------------------------------------------------------------------------------------
 void WriteResults(const char* file_names[])
 {
-	// необходимо определить функцию
+	// Г­ГҐГ®ГЎГµГ®Г¤ГЁГ¬Г® Г®ГЇГ°ГҐГ¤ГҐГ«ГЁГІГј ГґГіГ­ГЄГ¶ГЁГѕ
 	double _xmin, _xmax, _w, _h;
 	ofstream fout(file_names[0]);
-	fout << "x_min"  << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
+	//fout << "x_min" << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
 	for (int i = 0; i < solution.size(); i++) {
 		solution[i].GetParameters(_xmin, _xmax, _w, _h);
-		fout << _xmin << "\t" << _xmax << "\t" << _w << "\t" << _h << endl;
+		fout << _xmin << " " << _xmax << " " << _w << " " << _h << endl;
 	}
 	fout.close();
 
 	ofstream fout1(file_names[1]);
-	fout1 << "x_min" << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
+	//fout1 << "x_min" << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
 	for (int i = 0; i < not_solution.size(); i++) {
 		not_solution[i].GetParameters(_xmin, _xmax, _w, _h);
-		fout1 << _xmin << "\t" << _xmax << "\t" << _w << "\t" << _h << endl;
+		fout1 << _xmin << " " << _xmax << " " << _w << " " << _h << endl;
 	}
 	fout1.close();
 
 	ofstream fout2(file_names[2]);
-	fout2 << "x_min" << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
+	//fout2 << "x_min" << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
 	for (int i = 0; i < boundary.size(); i++) {
 		boundary[i].GetParameters(_xmin, _xmax, _w, _h);
-		fout2 << _xmin << "\t" << _xmax << "\t" << _w << "\t" << _h << endl;
+		fout2 << _xmin << " " << _xmax << " " << _w << " " << _h << endl;
 	}
 	fout2.close();
 }
